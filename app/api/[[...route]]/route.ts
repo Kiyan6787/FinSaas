8f4636d4a9c7eebfc5007f20cf811793a7ba75b9
Kiyan6787 <kiyanramjanum94@gmail.com>
2024-11-12 11:14:27 +0200

    Initial commit

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,26 +1,25 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-import { z } from "zod";
-import { zValidator } from "@hono/zod-validator";
-import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+
+import accounts from "./accounts";
+import categories from "./categories"
+import transactions from "./transactions"
+
 
 export const runtime = 'edge';
 
-const app = new Hono().basePath('/api')
+const app = new Hono().basePath('/api');
 
-app.get(
-    '/hello', 
-    clerkMiddleware(),
-    (c) => {
-        const auth = getAuth(c);
 
-        if (!auth?.userId) {
-            return c.json({error: "Unauthorised"});
-        }
-    return c.json({
-        message: "Hello Next.js",
-    })
-})
+const routes = app
+    .route("/accounts", accounts)
+    .route("/categories", categories)
+    .route("/transactions", transactions);
 
-export const GET = handle(app)
-export const POST = handle(app)
+
+export const GET = handle(app);
+export const POST = handle(app);
+export const PATCH = handle(app);
+export const DELETE = handle(app);
+
+export type AppType = typeof routes;
